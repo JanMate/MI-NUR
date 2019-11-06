e992a38ff8103082857b559736a10c6d72d0d342
Jan Matějka <matejj32@fit.cvut.cz>
2019-11-06 18:11:32 +0100

    Initial Commit

---
+++ MI-NUR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MI-NUR
+//
+//  Created by Jan Matějka on 06/11/2019.
+//  Copyright © 2019 Jan Matějka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
